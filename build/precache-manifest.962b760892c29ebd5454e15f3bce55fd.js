eca9ba4b48334e0053add1133873859b34b0de2b
Shantanu Jaiswal <shantanujaiswal.mantralabsglobal.com>
2020-05-29 02:30:39 +0530

    publish

---
+++ build/precache-manifest.962b760892c29ebd5454e15f3bce55fd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b37845c838a43831763bef5cfb89db",
+    "url": "/taratari-react/index.html"
+  },
+  {
+    "revision": "d416baa36a6f9fc62fc5",
+    "url": "/taratari-react/static/css/main.7fd6f248.chunk.css"
+  },
+  {
+    "revision": "b0dede1a9256e3461946",
+    "url": "/taratari-react/static/js/2.00e2ed3d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/taratari-react/static/js/2.00e2ed3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d416baa36a6f9fc62fc5",
+    "url": "/taratari-react/static/js/main.0b474adb.chunk.js"
+  },
+  {
+    "revision": "da61f02846ed4fb29d4c",
+    "url": "/taratari-react/static/js/runtime-main.106bdcfa.js"
+  },
+  {
+    "revision": "4e54b84ca588f4e96df10b9c597c93bb",
+    "url": "/taratari-react/static/media/Screen Shot 2020-05-28 at 5.50.34 PM.4e54b84c.png"
+  },
+  {
+    "revision": "672b5635ff99e5dddf82989ef0737929",
+    "url": "/taratari-react/static/media/bmw-57e3d44049_1280.672b5635.jpg"
+  },
+  {
+    "revision": "518e1a0052e6c5779fb578d7f693853f",
+    "url": "/taratari-react/static/media/c8aa7a41b257939d2ee64cc128b297ec.518e1a00.jpg"
+  },
+  {
+    "revision": "3b2051fdff2453f5cbfa6906c8a3c3cc",
+    "url": "/taratari-react/static/media/ecommerce2-1.3b2051fd.png"
+  },
+  {
+    "revision": "bbebd881e83ab57db984494866e210b7",
+    "url": "/taratari-react/static/media/search.bbebd881.png"
+  }
+]);
