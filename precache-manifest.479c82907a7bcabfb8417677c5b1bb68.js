f3c69c1e6e2df4773cb5b380983aa7c7ee01fbea
Shantanu Jaiswal <shantanujaiswal@Shantanus-MacBook-Air.local>
2020-08-18 00:06:28 +0530

    Updates

---
+++ precache-manifest.479c82907a7bcabfb8417677c5b1bb68.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c3ecb59fb0fbafda04ed7b63a549be",
+    "url": "/taratari-react/index.html"
+  },
+  {
+    "revision": "aa24e15df7cd0cf09909",
+    "url": "/taratari-react/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "73369c9ccdfecd429bbc",
+    "url": "/taratari-react/static/css/main.1ac5196f.chunk.css"
+  },
+  {
+    "revision": "aa24e15df7cd0cf09909",
+    "url": "/taratari-react/static/js/2.48589b3a.chunk.js"
+  },
+  {
+    "revision": "90b3ec31311490f2854c9763750a1c7c",
+    "url": "/taratari-react/static/js/2.48589b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73369c9ccdfecd429bbc",
+    "url": "/taratari-react/static/js/main.809fe21b.chunk.js"
+  },
+  {
+    "revision": "da61f02846ed4fb29d4c",
+    "url": "/taratari-react/static/js/runtime-main.106bdcfa.js"
+  },
+  {
+    "revision": "4e54b84ca588f4e96df10b9c597c93bb",
+    "url": "/taratari-react/static/media/Screen Shot 2020-05-28 at 5.50.34 PM.4e54b84c.png"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/taratari-react/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "672b5635ff99e5dddf82989ef0737929",
+    "url": "/taratari-react/static/media/bmw-57e3d44049_1280.672b5635.jpg"
+  },
+  {
+    "revision": "518e1a0052e6c5779fb578d7f693853f",
+    "url": "/taratari-react/static/media/c8aa7a41b257939d2ee64cc128b297ec.518e1a00.jpg"
+  },
+  {
+    "revision": "fe79b5de99f0b8427102249f7fcdabc7",
+    "url": "/taratari-react/static/media/cars.fe79b5de.png"
+  },
+  {
+    "revision": "3b2051fdff2453f5cbfa6906c8a3c3cc",
+    "url": "/taratari-react/static/media/ecommerce2-1.3b2051fd.png"
+  },
+  {
+    "revision": "59e05e3a953442062cc69c1d671b2577",
+    "url": "/taratari-react/static/media/fashion.59e05e3a.jpg"
+  },
+  {
+    "revision": "58bd2668b6253cf3e682cadcd3b08a72",
+    "url": "/taratari-react/static/media/furniture.58bd2668.jpg"
+  },
+  {
+    "revision": "a02bfaa54f3b9abd9ab3dee36ae89d5c",
+    "url": "/taratari-react/static/media/hobbies.a02bfaa5.jpg"
+  },
+  {
+    "revision": "2b941e03a74d5c4c04e1b081f80042e7",
+    "url": "/taratari-react/static/media/house.2b941e03.jpg"
+  },
+  {
+    "revision": "17bb76867b6db97d1847a7dc13b6d4c7",
+    "url": "/taratari-react/static/media/job.17bb7686.jpg"
+  },
+  {
+    "revision": "793419552519abf4f881f7d999fefdbc",
+    "url": "/taratari-react/static/media/main_image.79341955.jpeg"
+  },
+  {
+    "revision": "80972488f708301451eee3dbca304170",
+    "url": "/taratari-react/static/media/mobile.80972488.jpg"
+  },
+  {
+    "revision": "b23faf49e409b7a518e61b27a8787db1",
+    "url": "/taratari-react/static/media/online-store-57e6d24743_1280.b23faf49.png"
+  },
+  {
+    "revision": "5256f71f7924cad40ffc5d43f2bd3c7e",
+    "url": "/taratari-react/static/media/others.5256f71f.jpg"
+  },
+  {
+    "revision": "d553bea526a424bb11fc8099deb22975",
+    "url": "/taratari-react/static/media/pets.d553bea5.jpg"
+  },
+  {
+    "revision": "bbebd881e83ab57db984494866e210b7",
+    "url": "/taratari-react/static/media/search.bbebd881.png"
+  },
+  {
+    "revision": "c7a91822017e37c6d5e33d1a681a8bc5",
+    "url": "/taratari-react/static/media/service.c7a91822.jpg"
+  },
+  {
+    "revision": "2ecc07ea979e473ae6323cb6648de5ca",
+    "url": "/taratari-react/static/media/television.2ecc07ea.jpg"
+  }
+]);
