64cb2cbd4ea569eabe93ba6c305e9ecb54fbffa6
Shantanu Jaiswal <shantanujaiswal@Shantanus-MacBook-Air.local>
2020-08-17 23:59:59 +0530

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/taratari-react/precache-manifest.a32ad8a96e17882f217be8f63d8b2a21.js"
+  "/taratari-react/precache-manifest.479c82907a7bcabfb8417677c5b1bb68.js"
 );
 
 self.addEventListener('message', (event) => {
